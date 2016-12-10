33d76f1f6f2dd7d0bf492cd4974b79d2a509c811
Jonathan Chan <cusxio@gmail.com>
2016-12-10 21:59:24 +0800

    add progressive image loading

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,24 @@
+const images = document.querySelectorAll('.student__img');
+
+[].forEach.call(images, (img) => {
+    const tempImg = new Image();
+    tempImg.src = img.src;
+
+    tempImg.onload = () => {
+        img.classList.add('pre-loaded');
+    };
+
+    const data = img.getAttribute('data-src');
+    const largeImg = new Image();
+    largeImg.src = data;
+    largeImg.onload = () => {
+        img.src = data;
+        img.classList.remove('pre-loaded');
+        img.classList.add('large-loaded');
+        img.style.willChange = 'auto';
+    };
+});
+
 // https://webpack.github.io/docs/hot-module-replacement.html
 if (module.hot) {
     module.hot.accept();
