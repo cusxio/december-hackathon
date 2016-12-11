8458b319d0c3fdd1ff824270d61ff5c824c52cf1
Jonathan Chan <cusxio@gmail.com>
2016-12-11 10:25:32 +0800

    mobile modal

--- gulpfile.babel.js/tasks/watch.js
+++ gulpfile.babel.js/tasks/watch.js
@@ -3,13 +3,13 @@ import gulp from 'gulp';
 import config from '../config';
 
 const glob = {
-    scripts: path.join(config.css.src, '/**/*.{sass,scss,css}'),
+    styles: path.join(config.css.src, '/**/*.{sass,scss,css}'),
     html: path.join(config.html.src, '/**/*.html'),
     images: path.join(config.images.src, '/**/*.{jpg,png,svg,gif}'),
 };
 
 export default function watch() {
-    gulp.watch([glob.scripts], ['styles']);
+    gulp.watch([glob.styles], ['styles']);
     gulp.watch([glob.html], ['html']);
     gulp.watch([glob.images], ['images']);
 }
